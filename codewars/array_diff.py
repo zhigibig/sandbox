840c9d6978be121fcf1509242e21bd6e29cc3e2f
mr_libertariat <95529471+zhigibig@users.noreply.github.com>
2022-12-10 21:40:01 +0300

    Update array_diff.py

--- codewars/array_diff.py
+++ codewars/array_diff.py
@@ -1,15 +1,6 @@
 def array_diff(a, b):
     #task https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
     
-    #Your goal in this kata is to implement a difference function,
-    #which subtracts one list from another and returns the result.
-    
-    #It should remove all values from list a,
-    #which are present in list b keeping their order.
-    
-    #If a value is present in b, 
-    #all of its occurrences must be removed from the other
-    
     def del_same_el_from_list(arr):
         new_list = []
 
